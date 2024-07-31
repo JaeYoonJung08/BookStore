13f1771b86492daeb5456cf463be55177ed13920
JaeYoonJung08 <whitecy01@naver.com>
2024-08-01 06:39:35 +0900

    feat: 도서 추가 기능

--- routes/booklist.js
+++ routes/booklist.js
@@ -15,6 +15,67 @@ router.get('/', async (req, res) => {
 });
 
 
+//책 추가
+router.post('/bookAdd', async (req, res) => {
+    const {bookName, bookCount, bookPrice} = req.body;
+
+
+    console.log(bookName, bookCount, bookPrice);
+    
+    try{
+        
+        const checkbook = await req.db.query(
+            'select book_id, book_count from booklist where book_name = ?',
+            [bookName]
+        )
+        console.log(checkbook);
+        if (checkbook.length === 0) 
+        {
+            //책이 없으니 추가
+            await req.db.query(
+                'insert into booklist(book_name, book_count, book_price) values (?,?,?)',
+                [bookName, parseInt(bookCount), parseInt(bookPrice)]
+            )
+
+            return res.send(
+                `<script type="text/javascript">
+                alert("책이 추가 되었습니다.");
+                location.href='/';
+                </script>`
+            );
+
+        }
+        else 
+        {
+            //책이 있으면 Count 그만큼 추가해주기
+            const sumCount = checkbook[0].book_count;
+            var book_id = checkbook[0].book_id;
+            
+            var sum = parseInt(sumCount) + parseInt(bookCount);
+            console.log("bookcCount : ", sum);
+
+
+
+            const InsertSQL = await req.db.query(
+                'update booklist set book_count = ? where book_id = ?',
+                [sum, book_id]
+            )
+
+            return res.send(
+                `<script type="text/javascript">
+                alert("책이 수량이 추가 되었습니다.");
+                location.href='/';
+                </script>`
+            );
+
+        }
+
+    }
+    catch (error){
+        console.log(error);   
+    }
+})
+
 
 
 module.exports = router;
