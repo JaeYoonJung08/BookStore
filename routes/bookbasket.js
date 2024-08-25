70579dfe61e48f6075a0a602621699cbda338d97
JaeYoonJung08 <whitecy01@naver.com>
2024-08-26 02:39:30 +0900

    Feat: 코드 리펙토링, 주문보기 페이지 기능 추가

--- routes/bookbasket.js
+++ routes/bookbasket.js
@@ -24,7 +24,6 @@ router.get('/', async (req, res) => {
                 'select * from basketlist where basket_id = (select basket_id from bookbasket where user_id = ?)',
                 [req.session.user_id]
             )    
-            console.log("bookbasketlist : ", bookbasketlist);
             // 장바구니에 책이 있는 경우
             if (bookbasketlist.length > 0) {
                 req.session.basket_id = bookbasketlist[0].basket_id;
@@ -39,24 +38,130 @@ router.get('/', async (req, res) => {
                 );
 
                 console.log("books : " , books);
-                return res.render('bookbasket', { books, bookbasketlist });
+                return res.render('basket/bookbasket', { books, bookbasketlist });
             }
             else 
             {
-                return res.render('bookbasket', { books: [], bookbasketlist: [] });
+                return res.render('basket/bookbasket', { books: [], bookbasketlist: [] });
             }
-
-            // return res.render('bookbasket');
         }
         catch(error)
         {
             console.log(error);
-
         }
-
     }
 })
 
+
+router.post('/order', async (req, res) => {
+    logger.info(`Request received for URL: ${req.originalUrl}`);
+    const selectedBooks = req.body.selectedBooks;
+    const all_price = req.body.all_price;
+
+    console.log("selectedBooks : " , selectedBooks);
+    console.log("all_price : " , all_price);
+
+    //선택된 책이 없을 때
+    if (!selectedBooks) {
+        return res.send(
+            `<script type="text/javascript">
+            alert("주문할 책을 선택해주세요.");
+            location.href='/bookbasket';
+            </script>`);
+    }
+    const selectedBookList = Array.isArray(selectedBooks) ? selectedBooks.map(book => JSON.parse(book)) : [JSON.parse(selectedBooks)];
+
+    try {
+        //책 조회
+        // const books = await req.db.query(
+        //     'SELECT * FROM booklist WHERE book_id IN (?)',
+        //     [selectedBookIds]
+        // );
+        //기본 배송지, 상세 배송지, 우편 번호
+        const UserAddr = await req.db.query(
+            'select * from addr where user_id = ?',
+            [req.session.user_id]
+        )
+        //카드번호, 카드 종류, 카드 유효기간 
+        const UserCard = await req.db.query(
+            'select * from card wherer where user_id = ?',
+            [req.session.user_id]
+        )
+
+        console.log("UserAddr : " , UserAddr)
+        console.log("UserCard : " , UserCard)
+
+        res.render('basket/orderpage', {all_price, UserAddr, UserCard, selectedBookList});
+    } catch (error) {
+        console.log(error);
+        res.send(
+            `<script type="text/javascript">
+            alert("주문 처리 중 오류가 발생했습니다.");
+            location.href='/';
+            </script>`);
+    }
+});
+
+router.post('/add', async (req, res) => {
+    logger.info(`Request received for URL: ${req.originalUrl}`);
+    const {selectedBookList, totalPrice, selectedCard, selectedAddress} = req.body;
+    console.log("totalPrice : ", totalPrice);
+    console.log("selectedCard : ", selectedCard);
+    console.log("selectedAddress : ", selectedAddress);
+    console.log("selectedBookList : ", selectedBookList);
+
+    try{
+        const valueAddr = await req.db.query(
+            'select basic_add, detail_add, postal_code from addr where addr_id = ?', 
+            [selectedAddress]
+        )
+
+        const valueCard = await req.db.query(
+            'select card_number, type_card, expriation_time from card where card_id = ?', 
+            [selectedCard]
+        )
+        const { basic_add, detail_add, postal_code } = valueAddr[0];
+        const { card_number, type_card, expriation_time } = valueCard[0];
+        
+        //order 테이블에 값 넣기
+        const insertOrderQuery = `
+        INSERT INTO orders (
+            user_id, all_price, basic_add, detail_add, postal_code, card_number, type_card, expriation_time
+        ) VALUES (?, ?, ?, ?, ?, ?, ?, ?)`;
+        
+        const result = await req.db.query(insertOrderQuery, [
+            req.session.user_id, totalPrice, basic_add, detail_add, postal_code, card_number, type_card, expriation_time
+        ]);
+        console.log('Order inserted successfully ', result);
+        const orders_id = result.insertId;
+        
+        //여기서 부터 orderlist
+        const selectedBooks = JSON.parse(selectedBookList);
+        for (const book of selectedBooks) {
+            const insertOrderListQuery = `
+                INSERT INTO orderlist (orders_id, book_id, orderlist_count)
+                VALUES (?, ?, ?)
+            `;
+            await req.db.query(insertOrderListQuery, [orders_id, book.book_id, book.book_count]);
+        }
+        res.send(
+            `<script type="text/javascript">
+            alert("주문이 성공적으로 처리되었습니다.");
+            location.href='/';
+            </script>`
+        );
+
+    }
+    catch(error){
+        console.log(error);
+        res.send(
+            `<script type="text/javascript">
+            alert("주문 처리 중 오류가 발생했습니다.");
+            location.href='/';
+            </script>`
+        );
+    }
+})
 
 
 
